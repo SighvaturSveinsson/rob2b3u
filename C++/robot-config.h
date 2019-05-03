5d7edc165f0ca9f22a850fff12b9544ae7888816
SighvaturSveinsson <sighvatursv373@nemi.tskoli.is>
2019-05-03 08:58:35 +0000

    Create robot-config.h

---
+++ C++/robot-config.h
@@ -0,0 +1,7 @@
+vex::brain Brain;
+vex::motor LeftMotor = vex::motor(vex::PORT11);
+vex::motor RightMotor = vex::motor(vex::PORT20, true);
+vex::motor ArmMotor = vex::motor(vex::PORT18);
+vex::motor ClawMotor = vex::motor(vex::PORT16);
+vex::motor TurnTableMotor = vex::motor(vex::PORT17);
+vex::controller Controller1 = vex::controller();
